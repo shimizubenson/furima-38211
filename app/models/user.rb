f7913099a813e6233b8a29e742e48e9c39479e03
shimizubenson <benson69.smz@gmail.com>
2022-08-03 10:12:35 +0900

    単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
          validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :birthday, presence: true
-
+         
          VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
          validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
