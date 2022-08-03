af4f1586d0d7fc567ca5699fa089279c071d2f2f
shimizubenson <benson69.smz@gmail.com>
2022-08-03 21:01:16 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,8 +12,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :description, :status_id, :category_id, :region_id,:day_to_ship_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :status_id, :category_id, :region_id,:day_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
-
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,5 @@ class Item < ApplicationRecord
          validates :price,             presence: true
          
          belongs_to :user
+         has_one_attached :image
 end
