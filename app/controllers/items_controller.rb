91f57abc7f6e532431c682671eb6197d1dcdd24b
shimizubenson <benson69.smz@gmail.com>
2022-08-03 19:11:42 +0900

    itemテーブル作成~バリデーションまで実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :status_id, :category_id, :region_id,:day_to_ship_id, :price).merge(user_id: current_user.id)
+  end
+
+
 end
---
+++ app/models/item.rb
@@ -0,0 +1,13 @@
+class Item < ApplicationRecord
+
+         validates :name,              presence: true
+         validates :description,       presence: true
+         validates :status_id,         presence: true
+         validates :category_id,       presence: true 
+         validates :postage_id,        presence: true 
+         validates :region_id,         presence: true
+         validates :day_to_ship_id,    presence: true
+         validates :price,             presence: true
+         
+         belongs_to :user
+end
