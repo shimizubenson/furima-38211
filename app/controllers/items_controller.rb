9eca072b7be54fdb5720678fa7da5c76640a2778
shimizubenson <benson69.smz@gmail.com>
2022-08-04 21:12:35 +0900

    エラーハンドリングの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
   def index
-    @item = Item.new
+    @item = Item.all
   end
 
   def new
@@ -9,13 +9,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to  action: :index
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :status_id, :category_id, :region_id,:day_to_ship_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :status_id, :category_id, :postage_id, :region_id,:day_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,13 @@
 class Item < ApplicationRecord
 
-         validates :name, :description, :price,            presence: true
+         validates :name, :description,             presence: true
          validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
          validates :category_id, numericality: { other_than: 1,message: "can't be blank" }
          validates :postage_id, numericality: { other_than: 1,message: "can't be blank" }
          validates :region_id, numericality: { other_than: 1,message: "can't be blank" }
          validates :day_to_ship_id, numericality: { other_than: 1,message: "can't be blank" } 
-         
+         validates :price, presence: true, numericality: { only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+         extend ActiveHash::Associations::ActiveRecordExtensions
          belongs_to :user
          belongs_to :status
          belongs_to :category
